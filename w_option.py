6398b3f033dc5698bc1a3bdc22476a2b9e38ba35
Bobomurod2004 <bobomurodgoibnazarov@gmail.com>
2025-01-22 19:28:54 +0500

    4qo'shish

--- w_option.py
+++ w_option.py
@@ -1,3 +1,6 @@
-with open ("file.txt", "w") as file:
-    file.write("salommatmisiz.\n")
-    file.write("isjlaringiz yaxshimi.\n")
+# with open ("file.txt", "w") as file:
+#     file.write("salommatmisiz.\n")
+#     file.write("isjlaringiz yaxshimi.\n")
+with open ("file.txt", "a") as file:
+    file.write("Bu yangi so'zlar\n")
+    file.write("bu yangichalardir")
