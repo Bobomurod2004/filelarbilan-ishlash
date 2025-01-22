4276dfc66c3c6e7582fc08fbf5caf3eaa9772421
Bobomurod2004 <bobomurodgoibnazarov@gmail.com>
2025-01-22 20:00:07 +0500

    7qo'shish

--- os_module.py
+++ os_module.py
@@ -1,9 +1,15 @@
 import os
+# file_path="file.txt"
+# if os.path.exists(file_path):
+#     os.remove(file_path)
+#     print(f"file{file_path} o'chirildi")
+# else:
+#     print("fayl mavjud emas")
 
+dir_path="/bu yega beriladigan manzil"
 
-file_path="file.txt"
-if os.path.exists(file_path):
-    os.remove(file_path)
-    print(f"file{file_path} o'chirildi")
+if os.path.exists(dir_path) and os.path.isdir(dir_path):
+    os.rmdir(dir_path)
+    print(f"Diraktoriy {dir_path} o'chirildi")
 else:
-    print("fayl mavjud emas")
+    print("xatolik yuz berdi bu yerda yer bo'sh emas")
