09fdcf1ec62bd3d49446ec1251e681d105078bb2
Bobomurod2004 <bobomurodgoibnazarov@gmail.com>
2025-01-23 16:30:51 +0500

    eng oxirgi o'zgarish

--- os_module.py
+++ os_module.py
@@ -1,15 +1,16 @@
 import os
-# file_path="file.txt"
-# if os.path.exists(file_path):
-#     os.remove(file_path)
-#     print(f"file{file_path} o'chirildi")
-# else:
-#     print("fayl mavjud emas")
 
-# dir_path="/bu yega beriladigan manzil"
-#
-# if os.path.exists(dir_path) and os.path.isdir(dir_path):
-#     os.rmdir(dir_path)
-#     print(f"Diraktoriy {dir_path} o'chirildi")
-# else:
-#     print("xatolik yuz berdi bu yerda yer bo'sh emas")
+file_path="C:/Users/bobom/OneDrive/Documents/tugarak.docx"
+if os.path.exists(file_path):
+    os.remove(file_path)
+    print(f"file{file_path} o'chirildi")
+else:
+    print("fayl mavjud emas")
+
+dir_path="/bu yega beriladigan manzil"
+
+if os.path.exists(dir_path) and os.path.isdir(dir_path):
+    os.rmdir(dir_path)
+    print(f"Diraktoriy {dir_path} o'chirildi")
+else:
+    print("xatolik yuz berdi bu yerda yer bo'sh emas")
